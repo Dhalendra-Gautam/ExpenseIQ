196cc7110e4384fbee246baebc0626b279d98f78
Dhalendra-Gautam <dhalendragautam09@gmail.com>
2026-04-30 13:21:25 +0530

    Worked on Sidebar.jsx and Framer motion

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { styles } from '../assets/dummyStyles';
 import Navbar from './Navbar';
+import Sidebar from "./Sidebar";
+import { useState } from 'react';
 
 const Layout = ({ onLogout, user }) => {//the props  passed to Layout will be passed to Navbar component
+    const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
     return (
         <div className={styles.layout.root}>
             <Navbar user={user} onLogout={onLogout} /> {/*calling Navbar component and passing user and onLogout as props*/}
+            <Sidebar user={user}
+                isCollapsed={sidebarCollapsed}
+                setIsCollapsed={setSidebarCollapsed} />
         </div>
     )
 }
